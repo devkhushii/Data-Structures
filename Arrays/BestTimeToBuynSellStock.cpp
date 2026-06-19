1706edd9dd67b317a94ae0680a5c3d07562a14c3
Khushi Rani <128570886+devkhushii@users.noreply.github.com>
2026-06-19 06:54:14 +0000

    two array solution added

---
+++ Arrays/BestTimeToBuynSellStock.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxProfit=0;
+        int minPrice=prices[0];
+        
+        for(int i=1;i<prices.size();i++){
+            minPrice=min(minPrice,prices[i]);
+            int profit=prices[i]-minPrice;
+            maxProfit=max(maxProfit,profit);    
+        }
+        return maxProfit;
+    }
+};
+
+int main() {
+  
+    vector<int> prices = {2, 1, 11, 15};
+
+    Solution sol;  // Create object
+
+    int ans = sol.maxProfit(prices);
+
+    cout << "Maximum Profit: " << ans << endl;
+
+    cout << endl;
+
+    return 0;
+}
